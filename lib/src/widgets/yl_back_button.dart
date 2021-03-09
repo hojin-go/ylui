0edbddd642ed2ee93cc564870157ba1c3acd7ab8
2mao <lihaozhen@lanehub.cn>
2021-03-09 22:29:14 +0800

    feat(back_button): 支持属性更多

--- lib/src/widgets/yl_back_button.dart
+++ lib/src/widgets/yl_back_button.dart
@@ -5,19 +5,22 @@ class YlBackButton extends StatelessWidget {
   final VoidCallback onTap;
   final Color color;
   final IconData icon;
+  final EdgeInsets padding;
 
   const YlBackButton(
-      {Key key, this.onTap, this.color = YlColors.black90, this.icon})
+      {Key key, this.onTap, this.color = YlColors.black90, this.icon, this.padding})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CupertinoButton(
-      child: Padding(
-        padding: const EdgeInsets.only(right: 16),
+      padding: padding ?? EdgeInsets.zero,
+      child: Container(
+        padding: EdgeInsets.only(right: 16),
         child: Icon(
           icon ?? CupertinoIcons.back,
           color: color,
+          size: 24,
         ),
       ),
       onPressed: onTap ?? () => Navigator.of(context).pop(),
