75d0cc9be02f0566d75c3a69a92092ccabe020f6
yep <peitaotom@gmail.com>
2022-11-09 13:50:27 +0800

    增加gradient

--- lib/src/widgets/popover/yl_popover.dart
+++ lib/src/widgets/popover/yl_popover.dart
@@ -6,150 +6,161 @@ class YlPopover extends StatelessWidget {
   final Widget? header;
   final Widget? body;
   final Widget? footer;
+  final Gradient? gradient;
 
-  const YlPopover({Key? key, this.body, this.header, this.footer})
+
+  const YlPopover(
+      {Key? key, this.body, this.header, this.footer, this.gradient})
       : super(key: key);
 
-  YlPopover.close(
-      {Key? key,
-      Widget? title,
-      Widget? subTitle,
-      this.body,
-      this.footer,
-      Widget? closeWidget,
-      VoidCallback? onClosed})
+  YlPopover.close({Key? key,
+    Widget? title,
+    Widget? subTitle,
+    this.body,
+    this.footer,
+    this.gradient,
+    Widget? closeWidget,
+    VoidCallback? onClosed})
       : this.header = Container(
-          alignment: Alignment.center,
-          constraints: BoxConstraints(minHeight: 64),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Builder(
-                  builder: (context) {
-                    var body = <Widget>[];
-                    if (title != null) {
-                      body.add(DefaultTextStyle(
-                        style: YlTextStyles.header5.copyWith(height: 1.3),
-                        child: title,
-                      ));
-                    }
-                    if (title != null && subTitle != null) {
-                      body.add(SizedBox(
-                        height: 6,
-                      ));
-                    }
-                    if (subTitle != null) {
-                      body.add(DefaultTextStyle(
-                        style: YlTextStyles.body4
-                            .copyWith(height: 1.3, color: YlColors.black50),
-                        child: subTitle,
-                      ));
-                    }
-                    return Container(
-                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: body,
-                      ),
-                    );
-                  },
+    alignment: Alignment.center,
+    constraints: BoxConstraints(minHeight: 64),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Builder(
+            builder: (context) {
+              var body = <Widget>[];
+              if (title != null) {
+                body.add(DefaultTextStyle(
+                  style: YlTextStyles.header5.copyWith(height: 1.3),
+                  child: title,
+                ));
+              }
+              if (title != null && subTitle != null) {
+                body.add(SizedBox(
+                  height: 6,
+                ));
+              }
+              if (subTitle != null) {
+                body.add(DefaultTextStyle(
+                  style: YlTextStyles.body4
+                      .copyWith(height: 1.3, color: YlColors.black50),
+                  child: subTitle,
+                ));
+              }
+              return Container(
+                padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: body,
                 ),
-              ),
-              CupertinoButton(
-                child: closeWidget ??
-                    Image.asset(
-                      'lib/assets/x-mark.png',
-                      package: 'flutter_ylui',
-                    ),
-                onPressed: onClosed,
-              ),
-              SizedBox(
-                width: 10,
-              )
-            ],
+              );
+            },
           ),
-        );
+        ),
+        CupertinoButton(
+          padding: EdgeInsets.only(left: 20,top: 20,right: 20),
+          child: closeWidget ??
+              Image.asset(
+                'lib/assets/x-mark.png',
+                package: 'flutter_ylui',
+              ),
+          onPressed: onClosed,
+        ),
+        SizedBox(
+          width: 10,
+        )
+      ],
+    ),
+  );
 
-  YlPopover.confirm(
-      {Key? key,
-      Widget? title,
-      Widget? subTitle,
-      this.body,
-      this.footer,
-      VoidCallback? onCanceled,
-      VoidCallback? onDone})
+  YlPopover.confirm({Key? key,
+    Widget? title,
+    Widget? subTitle,
+    this.body,
+    this.footer,
+    this.gradient,
+    VoidCallback? onCanceled,
+    VoidCallback? onDone})
       : this.header = Container(
-          margin: const EdgeInsets.only(top: 20),
-          child: Row(
-            children: [
-              CupertinoButton(
-                minSize: 0,
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  '取消',
-                  style: YlTextStyles.body1.copyWith(color: YlColors.black70),
-                ),
-                onPressed: onCanceled,
-              ),
-              Expanded(
-                child: Builder(builder: (context) {
-                  var body = <Widget>[];
-                  if (title != null) {
-                    body.add(DefaultTextStyle(
-                      style: YlTextStyles.b17(color: YlColors.black90),
-                      child: title,
-                    ));
-                  }
-                  if (subTitle != null) {
-                    body.add(DefaultTextStyle(
-                      style: YlTextStyles.body4
-                          .copyWith(height: 1.3, color: YlColors.black50),
-                      child: subTitle,
-                    ));
-                  }
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: body,
-                  );
-                }),
-              ),
-              CupertinoButton(
-                minSize: 0,
-                padding: EdgeInsets.only(right: 20),
-                child: Text(
-                  '完成',
-                  style: YlTextStyles.body1.copyWith(color: YlColors.branding1),
-                ),
-                onPressed: onDone,
-              )
-            ],
+    margin: const EdgeInsets.only(top: 20),
+    child: Row(
+      children: [
+        CupertinoButton(
+          minSize: 0,
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            '取消',
+            style: YlTextStyles.body1.copyWith(color: YlColors.black70),
           ),
-        );
+          onPressed: onCanceled,
+        ),
+        Expanded(
+          child: Builder(builder: (context) {
+            var body = <Widget>[];
+            if (title != null) {
+              body.add(DefaultTextStyle(
+                style: YlTextStyles.b17(color: YlColors.black90),
+                child: title,
+              ));
+            }
+            if (subTitle != null) {
+              body.add(DefaultTextStyle(
+                style: YlTextStyles.body4
+                    .copyWith(height: 1.3, color: YlColors.black50),
+                child: subTitle,
+              ));
+            }
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: body,
+            );
+          }),
+        ),
+        CupertinoButton(
+          minSize: 0,
+          padding: EdgeInsets.only(right: 20),
+          child: Text(
+            '完成',
+            style: YlTextStyles.body1.copyWith(color: YlColors.branding1),
+          ),
+          onPressed: onDone,
+        )
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
-          color: YlColors.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-      child: SafeArea(
-        top: false,
-        bottom: true,
-        child: Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              header ??
-                  Container(
-                    height: 44,
-                  ),
-              if (body != null) body!,
-              if (footer != null) footer!
-            ],
+        color: YlColors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20), topRight: Radius.circular(20)),),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+            color: YlColors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            gradient: gradient),
+        child: SafeArea(
+          top: false,
+          bottom: true,
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                header ??
+                    Container(
+                      height: 44,
+                    ),
+                if (body != null) body!,
+                if (footer != null) footer!
+              ],
+            ),
           ),
         ),
       ),
@@ -163,18 +174,18 @@ Future<T?> showYlPopover<T>(BuildContext context, YlPopover child,
     context: context,
     barrierDismissible: barrierDismissible,
     routeSettings: routeSettings,
-    builder: (context) => Dismissible(
-      direction: DismissDirection.down,
-      onDismissed: (direction) => Navigator.pop(context),
-      key: Key(''),
-      child: child,
-    ),
+    builder: (context) =>
+        Dismissible(
+          direction: DismissDirection.down,
+          onDismissed: (direction) => Navigator.pop(context),
+          key: Key(''),
+          child: child,
+        ),
   );
 }
 
 ///
-Future<bool?> showYlPlainPopover(
-  BuildContext context, {
+Future<bool?> showYlPlainPopover(BuildContext context, {
   RouteSettings? routeSettings,
   required String title,
   required String content,
